774e918e81f9d375620277122f6a34c6ec13b522
BrycePearce <bpearce@angelo.edu>
2017-08-27 18:12:50 -0500

    Finshed add() tests

--- TDD/testing-demo/src/app/06-services/todo.service.ts
+++ TDD/testing-demo/src/app/06-services/todo.service.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 export class TodoService { 
   constructor(private http: Http) { 
   }
-
+  // add() takes a todo object, and returns an observable. Which we will fake in our spec with callFake.
   add(todo) {
     return this.http.post('...', todo).map(r => r.json());
   }
--- TDD/testing-demo/src/app/06-services/todos.component.spec.ts
+++ TDD/testing-demo/src/app/06-services/todos.component.spec.ts
@@ -1,10 +1,15 @@
+// arrange, act, assertion
+
 import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/throw';
 
 describe('TodosComponent', () => {
+  // this is testing ngOnInit
   let component: TodosComponent;
   let service: TodoService
 
@@ -18,19 +23,53 @@ describe('TodosComponent', () => {
   });
 
   it('should set todos property with the items returned from the server', () => {
-    spyOn(service, 'getTodos').and.callFake(() => { // simulating observable that is returned from the service (server). In this case, todo items
-      return Observable.from([ [
+    spyOn(service, 'getTodos').and.callFake(() => { // simulating observable that is returned from the service (server). In this case, todo items.
+      return Observable.from([[
         { id: 1, title: 'a' },
         { id: 2, title: 'b' },
         { id: 3, title: 'c' }
-      ] ]);
+      ]]);
     });
 
     component.ngOnInit(); // act
-    console.log(component.todos);
-    expect(component.todos.length).toBe(3);
+
+    expect(component.todos.length).toBe(3); // Assertion. 
   });
 
 
-  // seond test, for add()
+  // second test, for add(), making sure that the add service is called.
+  it('should call the server to save the changes when a new todo item is added', () => {
+    let spy = spyOn(service, 'add').and.callFake(t => { // we use spy, because we don't really want to call the server, just ensure that service.add is called. We use callFake to do this (see details in todo.service) ; t = todo
+      return Observable.empty(); // Again, we don't care about what's returned from the observable. We just want to make sure the add method is called, so we do .empty()
+    });
+
+    component.add(); // act (test)
+
+    expect(spy).toHaveBeenCalled(); // Assertion. We test to see if add() has been called
+  });
+
+
+  // In this test we check if add() appropriately handles adding the returned todo object, to our array. (pushed on on L17 in component.ts)
+  it('should add the new todo returned from the server', () => {
+    let todo = { id: 1 }; // fake todo response object. We don't care what the service actually returns, we are just making sure whatever it is gets added correctly.
+
+    let spy = spyOn(service, 'add').and.returnValue(Observable.from([todo])); // we could use callFake instead of returnValue, but this is a bit cleaner.
+
+    component.add(); // act (test)
+
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1); // ensuring that todo object that is returned from the server, is in our array.
+  });
+
+
+
+  // In this test, we are creating a test case to make sure our error case works, while adding a new todo. (E.g. L18 in component.ts)
+  // As always, we are just testing to make sure the error code is working. 
+  it('should set the message property if server returns an error when adding a new todo', () => {
+    let error = 'error from the server'; // spoof an error message that the service might respond with
+    let spy = spyOn(service, 'add').and.returnValue(Observable.throw(error)); // simulate the throwing of an error message. It "throws", so it knows it's an error.
+
+    component.add(); // act (test)
+
+    expect(component.message).toBe(error); // make sure the returned error is what we expect it is.
+  });
 });
