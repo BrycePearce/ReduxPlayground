88c4d7595b123c551f1d9ac3b979640c6a166f32
BrycePearce <bpearce@angelo.edu>
2017-08-27 20:18:34 -0500

    testing for delete

--- TDD/testing-demo/src/app/06-services/todos.component.spec.ts
+++ TDD/testing-demo/src/app/06-services/todos.component.spec.ts
@@ -39,7 +39,7 @@ describe('TodosComponent', () => {
 
   // second test, for add(), making sure that the add service is called.
   it('should call the server to save the changes when a new todo item is added', () => {
-    let spy = spyOn(service, 'add').and.callFake(t => { // we use spy, because we don't really want to call the server, just ensure that service.add is called. We use callFake to do this (see details in todo.service) ; t = todo
+    let spy = spyOn(service, 'add').and.callFake(t => { // we use spy on service.add, because we don't really want to call the server, just ensure that service.add is called. We use callFake to do this (see details in todo.service) ; t = todo
       return Observable.empty(); // Again, we don't care about what's returned from the observable. We just want to make sure the add method is called, so we do .empty()
     });
 
@@ -71,5 +71,40 @@ describe('TodosComponent', () => {
     component.add(); // act (test)
 
     expect(component.message).toBe(error); // make sure the returned error is what we expect it is.
+  });
+
+
+
+
+  // delete, if the user confirms
+  it('should call the server to delete a todo item, if the user confirms', () => {
+    // Arrange
+    spyOn(window, 'confirm').and.returnValue(true); // since we don't want to actually create a pop-up window when running unit tests, so this method listens for the confirm, and if confirmed, it calls the server.
+    // this spy is in our service, which is delete
+    let spy = spyOn(service, 'delete').and.returnValue(Observable.empty()); // returning empty observable, because we don't care what the server actually returns
+
+    // Act
+    component.delete(1); // e.g. ID:1
+
+    // Assertion ()
+    // expect(spy).toHaveBeenCalled(); (this works, but is too general)
+    expect(spy).toHaveBeenCalledWith(1); // this makes sure that the 1 we send from our Act, is the ID is going to the delete method of our service as well.
+  });
+
+
+
+
+  // test to make sure if user cancels the delete, that it does not delete anything
+  it('should NOT call the server to delete a todo item, if the user cancels', () => {
+    // Arrange
+    spyOn(window, 'confirm').and.returnValue(false); // since we don't want to actually create a pop-up window when running unit tests, so this method listens for the confirm, and if confirmed, it calls the server.
+    // this spy is in our service, which is delete
+    let spy = spyOn(service, 'delete').and.returnValue(Observable.empty()); // returning empty observable, because we don't care what the server actually returns
+
+    // Act
+    component.delete(1); // e.g. ID:1
+
+    // Assertion ()
+    expect(spy).not.toHaveBeenCalled(); // this makes sure that the 1 we send from our Act, is the ID is going to the delete method of our service as well.
   });
 });
--- TDD/testing-demo/src/app/06-services/todos.component.ts
+++ TDD/testing-demo/src/app/06-services/todos.component.ts
@@ -1,25 +1,26 @@
 
 import { TodoService } from './todo.service'
 
-export class TodosComponent { 
+export class TodosComponent {
   todos: any[] = [];
-  message; 
+  message;
 
-  constructor(private service: TodoService) {}
+  constructor(private service: TodoService) { }
 
-  ngOnInit() { 
+  ngOnInit() {
     this.service.getTodos().subscribe(t => this.todos = t);
   }
 
-  add() { 
+  add() {
     var newTodo = { title: '... ' };
     this.service.add(newTodo).subscribe(
       t => this.todos.push(t),
       err => this.message = err);
   }
 
+  // here we test: 1) delete the item if they are sure. 2) do not delete the item if they are not sure
   delete(id) {
-    if (confirm('Are you sure?'))
+    if (confirm('Are you sure?')) // this is a confirmation box
       this.service.delete(id).subscribe();
-  }  
+  }
 }
