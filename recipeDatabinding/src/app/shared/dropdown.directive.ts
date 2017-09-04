8da539ad83e60b6d96824b6ce62ce8213c468646
BrycePearce <bpearce@angelo.edu>
2017-09-04 17:01:47 -0500

    fixed drop down menus

---
+++ recipeDatabinding/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, HostListener, HostBinding } from '@angular/core';
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  public text: String;
+  @HostBinding('class.open') isOpen = false; // class.open is a bootstrap thing
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+  // Checks to see if the clicked element is one that has the [appDropdown] on it, then
+  // closes the dropdown if the clicked element was not selected.
+  @HostListener('document:click', ['$event']) clickout(event) {
+    if(!this.elRef.nativeElement.contains(event.target)) {
+      this.isOpen = false;
+    }
+   }
+  constructor(private elRef: ElementRef) { }
+}
