a549f1643a658e87eaecae2b40f2fb998af55fea
BrycePearce <bpearce@angelo.edu>
2017-10-01 17:45:00 -0500

    declaring and converting variables

--- GolangBasics/src/github.com/BrycePearce/firstapp/Main.go
+++ GolangBasics/src/github.com/BrycePearce/firstapp/Main.go
@@ -1,7 +1,63 @@
 package main
 
 import "fmt"
+import "strconv"
+
+// we can declare variables at the global level, but you have to use full syntax
+var x int = 90
 
 func main() {
 	fmt.Println("Hello Go!")
+
+	showPrintVars()
+	convertVar()
+
+}
+
+/*
+ * Assigning variables
+ */
+func showPrintVars() {
+	fmt.Println("Printing variables")
+	// Method 1
+	var a int
+	a = 42
+	fmt.Println(a)
+
+	// Method 2 (GO compiler will infer that b is an int, so don't need to declare int)
+	var b float32 = 42
+	fmt.Println(b)
+
+	// Method 3 (let GO compiler do all the work, this is the most common)
+	c := 16
+	fmt.Println(c)
+
+	fmt.Println(x) // global value
+
+	// Printing out var types
+	fmt.Printf("%v, %T", c, c) // we can see the format of the value and type of the number. Note that GO compiler assigns c to be an int
+}
+
+func convertVar() {
+	fmt.Println("\nConverting variable types")
+	var i int = 42
+	fmt.Printf("%v, %T\n", i, i)
+
+	var j float32
+	j = float32(i)
+	fmt.Printf("%v, %T\n", j, j)
+
+	var k string
+	k = string(i)
+	fmt.Printf("%v, %T\n", k, k) // outputs * string, which is the ascii for the number 42 as a string
+
+	// Better to use the standard library to convert variable types, shown below
+
+	var n int = 30
+	fmt.Printf("%v, %T\n", n, n)
+
+	var p string
+	p = strconv.Itoa(n)
+	fmt.Printf("%v, %T\n", p, p)
+
 }
