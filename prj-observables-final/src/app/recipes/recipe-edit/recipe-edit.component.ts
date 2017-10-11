4c4da0834cbb550bfc7b386365b8f8fd97b53475
BrycePearce <bpearce@angelo.edu>
2017-10-10 20:20:46 -0500

    add new ingredients while in edit mode functionality/html

--- prj-observables-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ prj-observables-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -29,6 +29,16 @@ export class RecipeEditComponent implements OnInit {
     console.log(this.recipeForm);
   }
 
+  // add ingredients while editing the recipe
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push( 
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    );
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
