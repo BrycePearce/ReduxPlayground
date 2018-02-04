96d4cbacea5620b11521789add001a2aec2dbedc
bpearce@angelo.edu <bpearce@angelo.edu>
2018-02-03 23:23:40 -0600

    boilerplate

---
+++ MvcCoreOverview/MvcCoreOverview/Startup.cs
@@ -0,0 +1,53 @@
+﻿// Startup has auto-configuration
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MvcCoreOverview
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            // default error page?
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            // Need this for static files to work
+            app.UseStaticFiles();
+
+            // Routes
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
