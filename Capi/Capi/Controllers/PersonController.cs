070302500e45718c72b9a4db8654b76c83299aa4
bpearce@angelo.edu <bpearce@angelo.edu>
2018-02-24 19:35:42 -0600

    edit view

--- Capi/Capi/Controllers/PersonController.cs
+++ Capi/Capi/Controllers/PersonController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Capi.data;
+using Capi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Capi.Controllers
@@ -16,10 +17,36 @@ namespace Capi.Controllers
         public PersonController(CrudContext context) {
             _context = context;
         }
-        // return a view with the list of people
+        // return a view with the list of people, looks for the Index.cshtml in the Person views folder
         public IActionResult Index()
         {
+            // ToList() converts array to IEnumerable list
             return View(_context.People.ToList());
+        }
+
+        // Logic for loading the Details page
+        public IActionResult Details(int? id) {
+            if (id == null) {
+                // automatically generated notfound page
+                return NotFound();
+            }
+            Person person = _context.People.SingleOrDefault(x => x.ID == id);
+            if (person == null) {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        // Logic for loading the Details page
+        public IActionResult Edit(int? id) {
+            if (id == null) {
+                return NotFound();
+            }
+            Person person = _context.People.SingleOrDefault(x => x.ID == id);
+            if (person == null) {
+                return NotFound();
+            }
+            return View(person);
         }
     }
 }
