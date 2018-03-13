94bf3d51ca90b72ddf59113eba06fba594ef9709
bpearce@angelo.edu <bpearce@angelo.edu>
2018-03-12 21:41:26 -0500

    basic observable examples

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { Observer } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +13,38 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
+    // Pt.2 Baisc example of firing an observable, and listening for the response.
+    // emit an observable every second
+    //  const myNumbers = Observable.interval(1000);
+
+    // Subscribe to the observable
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   });
+
+
+  //   // Pt.3 Creating your own observable and subscribing to it
+  //   // Setup the observable
+  //   const myObservable = Observable.create((observer: Observer<string>) => {
+  //     setTimeout(() => {
+  //       observer.next('first package'); // next tells the observable to move on, but do not have to have it here since the timeout will end after 2 seconds.
+  //     }, 2000);
+  //     setTimeout(() => {
+  //       observer.next('second package');
+  //     }, 4000);
+  //     setTimeout(() => {
+  //       // observer.error('this does not work');
+  //       observer.complete();
+  //     }, 5000);
+  //   });
+
+  //   // Subscribe to the observable
+  //   myObservable.subscribe(
+  //     (data: string) => { console.log(data); },
+  //     (error: string) => { console.log(error); },
+  //     () => { console.log('completed'); }
+  //   );
+  // }
 
 }
--- observables-start/src/app/user/user.component.ts
+++ observables-start/src/app/user/user.component.ts
@@ -10,14 +10,14 @@ export class UserComponent implements OnInit {
   id: number;
 
   constructor(private route: ActivatedRoute) { }
-
+  // subscribe to the route params and listen for changes emitted from app.component.ts
   ngOnInit() {
     this.route.params
       .subscribe(
         (params: Params) => {
+          // display the new ID in the /user page when it is updated
           this.id = +params['id'];
         }
       );
   }
-
 }
