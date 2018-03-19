9e84b315fc2666313c538297347838007c164840
bpearce@angelo.edu <bpearce@angelo.edu>
2018-03-18 20:55:38 -0500

    added GET request for individual cities based on ID

--- CityInfo/CityInfo/Controllers/CitiesController.cs
+++ CityInfo/CityInfo/Controllers/CitiesController.cs
@@ -4,14 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CityInfo.Controllers
-{
-    public class CitiesController : Controller
-    {
+namespace CityInfo.Controllers {
+    [Route("api/cities")]
+    public class CitiesController : Controller {
+        // return a list of all available cities when GET "api/cities" is requested 
         [HttpGet("api/cities")]
-        public JsonResult GetCities()
-        {
+        public JsonResult GetCities() {
             return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        // return a single city (don't need api/cities/{id}, since api/cities is already part of the get route)
+        // id will be automatically set from the URI
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id) {
+            return new JsonResult(
+            // firstordefault() will not return an exception if {id} is not provided, it will return null or the default value. first() will throw an exception.
+
+            /*
+             * If you returned CitiesDataStore.Current.Cities[id] here, it would be accessing it in the context of an array. E.g., GET an id of 1, and it would return and item with an id of 2
+             * Wheras, by using FirstOrDefault and the lamba expression is querying the data itself. E.g., GET and id of 1, and it would return the id of 1
+             * note: c here is populated by C# magic
+             */
+                CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id)
+            );
         }
     }
 }
