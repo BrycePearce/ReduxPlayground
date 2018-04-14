f7f047ed47393830c51e4f363a639173828f785c
bpearce@angelo.edu <bpearce@angelo.edu>
2018-04-14 17:26:15 -0500

    created mock database that stores all the items from our models/entities

---
+++ CityInfo/CityInfo/Entities/CityInfoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Entities
+{
+    public class CityInfoContext : DbContext
+    {
+        // If database exists, do nothing. If it doesn't, it will create it.
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options) {
+            Database.EnsureCreated();
+        }
+
+        // essentially the model
+        public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointsOfInterest  { get; set; }
+    }
+}
--- CityInfo/CityInfo/Startup.cs
+++ CityInfo/CityInfo/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CityInfo
@@ -16,6 +18,10 @@ namespace CityInfo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(); // adds CRUD operations context to your project
+
+            // add DB context for entity framework core local database
+            var connectionString = @"Server=(localdb)\mssqllocaldb;Database=CityInfoDB;Trusted_Connection=True;";
+            services.AddDbContext<CityInfoContext>(o => o.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +37,7 @@ namespace CityInfo
             app.UseStatusCodePages();
 
             app.UseMvc(); // http://blog.2mas.xyz/asp-net-5-adding-mvc-to-an-application/
+
             //app.Run(async (context) => {
             //    await context.Response.WriteAsync("Hello World!");
             //});
