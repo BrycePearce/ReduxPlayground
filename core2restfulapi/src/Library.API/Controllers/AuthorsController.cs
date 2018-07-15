0fbebd24e6d07f7b883728fb5be02999accdf4f4
bpearce@angelo.edu <bpearce@angelo.edu>
2018-07-14 21:28:02 -0500

    initial commit, minor setup

---
+++ core2restfulapi/src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,22 @@
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+        // constructor injection to get an instance of our repository
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+
+            // serialize authors to JSON, JsonResult formats given object to json
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
