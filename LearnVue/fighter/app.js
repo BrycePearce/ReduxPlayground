3ad697a745acde067e8fd70284b4ed87e4c91494
bpearce@angelo.edu <bpearce@angelo.edu>
2019-05-22 17:12:10 -0500

    styles, final attack/heal implementation

--- LearnVue/fighter/app.js
+++ LearnVue/fighter/app.js
@@ -3,39 +3,60 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame() {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack() {
             // player attack
-            this.monsterHealth -= this.calculateDamage(3, 10);
-
+            const damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
             if (this.checkWin()) {
                 return;
             }
             this.monsterAttacks();
         },
         specialAttack() {
-            this.monsterHealth -= this.calculateDamage(10, 20);
+            const damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player special attacks for ' + damage
+            });
             if (this.checkWin()) {
                 return;
             }
             this.monsterAttacks();
         },
         heal() {
-
+            this.playerHealth <= 90 ? this.playerHealth += 10 : this.playerHealth = 100;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
+            this.monsterAttacks();
         },
         giveUp() {
-
+            this.gameIsRunning = false;
         },
         monsterAttacks() {
-            this.playerHealth -= this.calculateDamage(5, 12);
+            const damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
             this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            });
         },
         calculateDamage(minDmg, maxDmg) {
             return Math.max(Math.floor(Math.random() * maxDmg) + 1, minDmg);
@@ -59,4 +80,4 @@ new Vue({
             return false;
         }
     }
-})
+});
