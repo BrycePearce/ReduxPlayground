63c5b6d558d2410366cd2e7e3c52840fd5633cc3
bpearce@angelo.edu <bpearce@angelo.edu>
2019-05-31 18:56:00 -0500

    vue-resource, post example

--- LearnVue/http-vue-resource/src/App.vue
+++ LearnVue/http-vue-resource/src/App.vue
@@ -1,16 +1,43 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Http</h1>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Http</h1>
+        <div class="form-group">
+          <label>Username</label>
+          <input v-model="user.username" class="form-control" type="text">
         </div>
+        <div class="form-group">
+          <label>Mail</label>
+          <input v-model="user.email" class="form-control" type="text">
+        </div>
+        <button class="btn btn-primary" @click="submit">Send</button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-    }
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        email: ""
+      }
+    };
+  },
+  methods: {
+      submit() {
+          this.$http.post('https://vuejs-http-demo-project.firebaseio.com/data.json', this.user)
+          .then((response) => {
+              console.log(response);
+          }, (error) => {
+              console.log(error)
+          });
+      }
+  }
+};
 </script>
 
 <style>
