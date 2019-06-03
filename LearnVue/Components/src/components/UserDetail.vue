245a46423376dd84045a54a9d84072709343394b
bpearce@angelo.edu <bpearce@angelo.edu>
2019-06-03 18:56:41 -0500

    pass data from parent -> child

--- LearnVue/Components/src/components/UserDetail.vue
+++ LearnVue/Components/src/components/UserDetail.vue
@@ -2,10 +2,26 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>Child component recieved name: {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
+export default {
+    // props: ['name'],
+    props: {
+        // name: [String, Array] // can be string or array
+        name: String,
+        required: true, // this component can only be used if this prop is passed
+        default: '' // you can also set default value
+        // default: function() { return {} } if you want default to be an object, or an array, or something
+    },
+    methods: {
+        switchName() {
+            return this.name.split('').reverse().join('');
+        }
+    }
+}
 </script>
 
 <style scoped>
