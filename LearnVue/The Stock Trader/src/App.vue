ce15ec30b10e356fcae2d423e2c6c99f12296851
bpearce@angelo.edu <bpearce@angelo.edu>
2019-06-11 21:43:13 -0500

    Added mock-stocks, mock-buy functionality

--- LearnVue/The Stock Trader/src/App.vue
+++ LearnVue/The Stock Trader/src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="container">
-    <app-header>
+    <app-header></app-header>
       <div class="row">
         <div class="col-xs-12">
           <router-view></router-view>
         </div>
       </div>
-    </app-header>
-  </div>
+        </div>
 </template>
 
 <script>
--- LearnVue/The Stock Trader/src/components/stocks/Stock.vue
+++ LearnVue/The Stock Trader/src/components/stocks/Stock.vue
@@ -1,3 +1,42 @@
 <template>
-    <h1>The Stocks - Stock Component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input v-model="quantity" type="number" class="form-control" placeholder="Quantity">
+        </div>
+        <div class="pull-right">
+          <button @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(quantity)" class="btn btn-success">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      console.log(order);
+      this.quantity = null;
+    }
+  }
+};
+</script>
