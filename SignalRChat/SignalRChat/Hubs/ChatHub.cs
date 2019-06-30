4aae74cd99fd91bead906ebf048b6e4309d557f8
bpearce@angelo.edu <bpearce@angelo.edu>
2019-06-30 14:47:34 -0500

    implemented SignalR with UI

--- SignalRChat/SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/SignalRChat/Hubs/ChatHub.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+// A hub is a class that serves as a high-level pipeline that handles client-server communication.
 namespace SignalRChat.Hubs
 {
-    public class ChatHub
+    public class ChatHub : Hub // The Hub class manages connections, groups, and messaging
     {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message); // send the message
+        }
     }
 }
