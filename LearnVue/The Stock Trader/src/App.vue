e54f97c7dd5b084ea478552ae38bcd172a769ac7
bpearce@angelo.edu <bpearce@angelo.edu>
2019-06-29 19:39:32 -0500

    end day, route transitions

--- LearnVue/The Stock Trader/src/App.vue
+++ LearnVue/The Stock Trader/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container">
     <app-header></app-header>
-      <div class="row">
-        <div class="col-xs-12">
+    <div class="row">
+      <div class="col-xs-12">
+        <transition name="slide" mode="out-in">
           <router-view></router-view>
-        </div>
+        </transition>
       </div>
-        </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -16,13 +18,43 @@ export default {
     appHeader: Header
   },
   created() {
-    this.$store.dispatch('initStocks');
+    this.$store.dispatch("initStocks");
   }
 };
 </script>
 
 <style>
 body {
-    padding: 30px;
+  padding: 30px;
+}
+
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
 }
 </style>
--- LearnVue/The Stock Trader/src/components/Header.vue
+++ LearnVue/The Stock Trader/src/components/Header.vue
@@ -13,6 +13,7 @@
       </ul>
       <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
       <ul class="nav navbar-nav navbar-right">
+        <li><a href="#" @click="endDay">End Day</a></li>
         <li class="dropdown">
           <a
             class="nav-link dropdown-toggle"
@@ -41,10 +42,19 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
   computed: {
     funds() {
       return this.$store.getters.funds;
+    }
+  },
+  methods: {
+    ...mapActions([
+      'randomizeStocks'
+    ]),
+    endDay() {
+      this.randomizeStocks();
     }
   }
 }
