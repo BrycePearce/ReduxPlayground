5d0dbf8a5db096beeda6c51f8f8f7dc1fa75e0f3
bpearce@angelo.edu <bpearce@angelo.edu>
2020-04-29 20:27:38 -0500

    added fly controls

--- threejs/script.js
+++ threejs/script.js
@@ -5,6 +5,13 @@ import {
     cube
 } from './cube.js';
 
+// Controls
+import {
+    FlyControls
+} from './node_modules/three/examples/jsm/controls/FlyControls.js';
+
+// todo: abstract these three things outside, use singleton pattern
+// https://alligator.io/js/js-singletons/
 /* 
  * To display anything with Three.js, we need a scene, camera, and renderer. So that we can render a scene. 
  */
@@ -13,48 +20,40 @@ const scene = new THREE.Scene();
 // Options: field of view, aspect ratio, near clipping plane, far clipping plane (deals with render distance)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
 
-// renders the above generated scene, as a canvas element on the document body
-document.body.appendChild(renderer.domElement);
+const controls = new FlyControls(camera, renderer.domElement);
+const clock = new THREE.Clock();
 
-// Add a cube to our scene, by default it sets it at 0,0,0 - which is where the camera is. We will not be ablve to see it by default
-scene.add(cube);
+init();
+animate();
 
-// Move the camera out so we can see the cube
-camera.position.z = 5;
+function init() {
+
+    // renders the above generated scene, as a canvas element on the document body
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // set up camera controls
+    controls.movementSpeed = 50;
+    controls.rollSpeed = Math.PI / 24;
+
+    // Add a cube to our scene, by default it sets it at 0,0,0 - which is where the camera is. We will not be ablve to see it by default
+    scene.add(cube);
+
+    // Move the camera out so we can see the cube
+    camera.position.z = 5;
+}
 
 // renders everything
 function animate() {
+    const delta = clock.getDelta();
     // rotate our cube, each time animate is called, it re-draws the scene, so at every re-draw instance it updates the position by 0.05
     cube.rotation.x += 0.03;
     cube.rotation.y -= 0.03;
 
+    controls.update(delta);
     // create a loop that draws, and re-draws the scene every time the screen is refreshed. (60 times per second on avg)
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
-
-// choppy move camera around
-function onDocumentKeyDown(event) {
-    const keyCode = event.which;
-    const moveSpeed = 0.5;
-    if (keyCode === 87) {
-        camera.position.y += moveSpeed;
-    } else if (keyCode === 83) {
-        camera.position.y -= moveSpeed;
-    } else if (keyCode === 65) {
-        camera.position.x -= moveSpeed;
-    } else if (keyCode === 68) {
-        camera.position.x += moveSpeed;
-    } else if (keyCode === 32) {
-        camera.position.set(0, 0, 0);
-    }
-};
-
-document.addEventListener("keydown", onDocumentKeyDown, false);
-
-
-animate();
