0ca8969f4727e643db235ebc42660d5ba154f379
bpearce@angelo.edu <bpearce@angelo.edu>
2020-05-03 19:40:24 -0500

    added stars

--- threejs/environment.js
+++ threejs/environment.js
@@ -3,12 +3,17 @@ import * as THREE from './node_modules/three/src/Three.js';
 import props from './config/reference.js';
 
 export default () => {
+    generateStars();
+    generateCubeWall();
+};
+
+function generateCubeWall() {
     let grid = new THREE.Object3D();
     for (let row = 0; row < 50; row++) {
         for (let col = 0; col < 50; col++) {
             const box = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), new THREE.MeshBasicMaterial({
                 color: '#106b8c',
-                wireframe: true,
+                wireframe: true
             }));
             box.position.x = (row - 10 / 2) * 10;
             box.position.y = (col - 10 / 2) * 10;
@@ -17,4 +22,30 @@ export default () => {
     }
     props.structure.cubeGrid = grid;
     props.scene.add(props.structure.cubeGrid);
-};
+}
+
+// if need to update in render, add to props.structure
+function generateStars() {
+    let vertices = [];
+
+    for (var i = 0; i < 10000; i++) {
+
+        let x = THREE.MathUtils.randFloatSpread(2000);
+        let y = THREE.MathUtils.randFloatSpread(2000);
+        let z = THREE.MathUtils.randFloatSpread(2000);
+
+        vertices.push(x, y, z);
+
+    }
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+
+    const material = new THREE.PointsMaterial({
+        color: 0x888888
+    });
+
+    const points = new THREE.Points(geometry, material);
+
+    props.wscene.add(points);
+}
