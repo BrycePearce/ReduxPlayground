ff751d0a95870c5078357b0eb8ae44c1fafce904
bpearce@angelo.edu <bpearce@angelo.edu>
2020-09-13 00:58:21 -0500

    finished header, added booking info section

--- airbnb/postcss.config.js
+++ airbnb/postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require("tailwindcss"),
+    require('postcss-import'),
     require("autoprefixer"),
     ...(process.env.NODE_ENV === "production" ? [require("cssnano")] : []),
   ],
