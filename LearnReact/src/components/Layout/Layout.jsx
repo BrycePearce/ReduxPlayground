520d28eea6cf5d484117fa3779785edc7e1edae6
bpearce@angelo.edu <bpearce@angelo.edu>
2020-12-12 20:59:18 -0600

    hamburger icon / toggle drawer

--- LearnReact/src/components/Layout/Layout.jsx
+++ LearnReact/src/components/Layout/Layout.jsx
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
 
-import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true,
+    showSideDrawer: false,
   };
 
   sideDrawerClosedHandler = () => {
     this.setState({ showSideDrawer: false });
   };
 
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+
   render() {
     return (
       <>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler}
