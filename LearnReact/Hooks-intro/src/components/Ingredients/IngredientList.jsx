725dbc4bcd5165f3c276a52e3d6a8317fc05e1e3
bpearce@angelo.edu <bpearce@angelo.edu>
2020-12-14 23:49:52 -0600

    useEffect

--- LearnReact/Hooks-intro/src/components/Ingredients/IngredientList.jsx
+++ LearnReact/Hooks-intro/src/components/Ingredients/IngredientList.jsx
@@ -8,11 +8,11 @@ const IngredientList = (props) => {
       <h2>Loaded Ingredients</h2>
       <ul>
         {props.ingredients.map((ig) => (
-          <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
+          <li key={ig.id}>
             <span>{ig.title}</span>
             <span>
               {ig.amount}{" "}
-              <button onClick={props.removeIngredientHandler}>x</button>
+              <button onClick={props.onRemoveItem.bind(this, ig.id)}>x</button>
             </span>
           </li>
         ))}
--- LearnReact/Hooks-intro/src/components/Ingredients/Ingredients.jsx
+++ LearnReact/Hooks-intro/src/components/Ingredients/Ingredients.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -8,11 +8,30 @@ import Search from "./Search";
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  // useCallback will cache the function, so that when the component re-renders, this specific function will not re-create. So that when passing this function as a prop
+  // it will not update the child component (causing it to re-render) and cause an infinite loop
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+    setUserIngredients(filteredIngredients);
+  }, []); // could pass filteredIngredients here, but don't actually have to
+
   const addIngredientHandler = (ingredient) => {
-    setUserIngredients((prevIngredients) => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    fetch(
+      "https://react-hooks-update-b0cfc-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
 
   const removeIngredientHandler = (ingredientId) => {
@@ -28,7 +47,7 @@ const Ingredients = () => {
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList
           ingredients={userIngredients}
           onRemoveItem={removeIngredientHandler}
